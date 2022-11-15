0e45d4c4c4011a2490e8de2e66702aba5e96191d
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-11-15 20:58:01 +0000

    modified:   src/routes/Projects.css 	modified:   src/routes/Projects.jsx

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,8 +1,8 @@
 import { Component, useState } from 'react'
-import './Projects.css' 
+import './Projects.css'
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [ count, setCount ] = useState(0);
   const addOne = () => {
     let value = count + 1;
     setCount(value);
@@ -12,44 +12,51 @@ const Counter = () => {
     setCount(value);
   };
 
-    return (
-      <>
+  return (
+    <>
       <div>
 
-      <div className="counter">
-        <h2 className='counter-title'>Counter</h2>
-        {count < 0 ? (
-          <h2 className="negative">{count}</h2>
+        <div className="counter">
+          <h2 className='counter-title'>Counter</h2>
+          {count < 0 ? (
+            <h2 className="negative">{count}</h2>
           ) : (
-          <h2 className="positive">{count}</h2>
+            <h2 className="positive">{count}</h2>
           )}
-        <button onClick={addOne} className="addOne">
-          +1{' '}
-        </button>
-        <button onClick={minusOne} className="minusOne">
-          -1{' '}
-        </button>
+          <button onClick={addOne} className="addOne">
+            +1{' '}
+          </button>
+          <button onClick={minusOne} className="minusOne">
+            -1{' '}
+          </button>
+        </div>
       </div>
-          </div>
-          </>
-    )
-  }
+    </>
+  )
+}
 
 // Forms with multiple input fields and useState for updating the state.
 const Form = () => {
-  const [form, setUpdates] = useState(initialState);
-  
+  const [ form, setUpdates ] = useState('');
+  const handleChange = (e) => {
+    const value = e.target.value
+    setUpdates(value)
+  }
 
 
-return (
-  <>
-  <form className="form" onSubmit={Form}>
-    <p className="typewriter-print"></p>
-    <label className="typewriter-label" htmlFor='typewriter'>Typewriter</label>
-    <textarea className="typewriter" placeholder="Type to print on the screen" rows="10"></textarea>
-  </form>
-  </>
-)
+
+  return (
+    <>
+      <div className="form">
+        <label className="typewriter-label" htmlFor='typewriter'>Typewriter</label>
+        <p className="typewriter-print">{form}</p>
+        <textarea className="typewriter"
+          type='text'
+          value={form}
+          onChange={handleChange} placeholder="Type to print on the screen" rows="10"></textarea>
+      </div>
+    </>
+  )
 }
 
 class ColorGenerator extends Component {
@@ -63,9 +70,9 @@ class ColorGenerator extends Component {
     }
     return (
       <div className='color__generator'>
-<div className='color'  style={style} onClick={this.props.update.bind(this, this.props.index)} >
-<p className='color_code' >{this.props.hexCode}</p>
-</div>
+        <div className='color' style={style} onClick={this.props.update.bind(this, this.props.index)} >
+          <p className='color_code' >{this.props.hexCode}</p>
+        </div>
       </div>
     )
   }
@@ -78,8 +85,8 @@ class ColorGenerate extends Component {
       colorCount: 6,
       colors: []
     }
-    for (let i = 0; i < this.state.colorCount; i+=1) {
-      this.state.colors.push({hexCode: this.generateColor()})
+    for (let i = 0; i < this.state.colorCount; i += 1) {
+      this.state.colors.push({ hexCode: this.generateColor() })
     }
   }
 
@@ -90,7 +97,7 @@ class ColorGenerate extends Component {
   updateColor(index) {
     let colors = this.state.colors.slice()
     const currentColor = this.generateColor()
-    colors[index].hexCode = currentColor
+    colors[ index ].hexCode = currentColor
     this.setState({
       colors: colors
     })
@@ -100,17 +107,17 @@ class ColorGenerate extends Component {
     return (
 
       <>
-      <div className="colors_main">
-    <h1>Color Generator</h1>
-    <div className='colors'>
-      {this.state.colors.map((color, index) => <ColorGenerator key={`color-${index}`} index={index} update={this.updateColor.bind(this)} hexCode={color.hexCode} className="colors_gen"></ColorGenerator>)}
-    </div>
-<p className='hint'>Hint: Tap on the hexcode to generate new colors and the colors to copy to clipboard.</p>
-      </div>
-    </>
+        <div className="colors_main">
+          <h1>Color Generator</h1>
+          <div className='colors'>
+            {this.state.colors.map((color, index) => <ColorGenerator key={`color-${index}`} index={index} update={this.updateColor.bind(this)} hexCode={color.hexCode} className="colors_gen"></ColorGenerator>)}
+          </div>
+          <p className='hint'>Hint: Tap on the hexcode to generate new colors and the colors to copy to clipboard.</p>
+        </div>
+      </>
 
-)
-}
+    )
+  }
 
 }
 
@@ -119,22 +126,22 @@ class Projects extends Component {
   render() {
     return (
       <>
-      <div className="projects">
-        <h1 className='header'>Projects Built With React</h1>
-        <div className='main__projects'>
-          <div className='project'>
-          <Counter />
-          </div>
+        <div className="projects">
+          <h1 className='header'>Projects Built With React</h1>
+          <div className='main__projects'>
+            <div className='project'>
+              <Counter />
+            </div>
 
-          <div className='project'>
-          <ColorGenerate />
-          </div>
+            <div className='project'>
+              <ColorGenerate />
+            </div>
 
-          <div className='project'>
-            {/* <Form/> */}
+            <div className='project'>
+              <Form />
+            </div>
           </div>
         </div>
-      </div>
       </>
     )
   }
