6a89ab3951a774a2049a556121f170e045d0e28c
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-11-15 20:30:15 +0000

    modified:   src/routes/Contact.css 	modified:   src/routes/Contact.jsx

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './Contact.css'
+import {MdConstruction} from 'react-icons/md'
 
 export default class Contact extends Component {
   render() {
     return (
       <>
-      <div className='contact'>This the Contact page.</div>
+      <div className='contact'>I am under Construction, Please check back later...<br/>
+      <MdConstruction className='constructor'/>
+      </div>
       </>
     )
   }
